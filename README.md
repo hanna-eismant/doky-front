286c981611617219a947d2bc87d9d5994633457d
Anton <kurguzenkov@gmail.com>
2024-02-27 16:31:32 +0300

    MAIN-T-69-Migrate-to-Webpack darft

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ LTS version of [Node.js](https://nodejs.org) should be installed. nvm is recomme
 ### Commands to Run Client
 
 * `npm install` - install dependencies. Need to execute before first run or when dependencies updated.
-* `npm start` - run client. Will open new tab in browser. Life reloading is working only for js changes.
+* `npm start` - run client. Will open new tab in browser.
   To apply other changes re-run this command. By default, api pointing to **dev** (remote) back-end
 * `npm run start:local`- run client with api pointing to **local** back-end
 * `npm run start:dev` - run client with api pointing to **dev** (remote) back-end.
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer')
+
+module.exports = (env, argv) => {
+  const beEnv = argv['be-env'] || 'dev';
+  return {
+    mode: argv.mode,
+    devtool: argv.mode === 'development'
+      ? 'source-map'
+      : false,
+    plugins:  [
+      new HtmlWebpackPlugin({ template: 'static/index.html' })
+    ],
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    resolve: {
+      alias: {
+        config: `./config.${beEnv}.js`
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jsx?)$/,
+          exclude: /(node_modules)/,
+          loader: "babel-loader",
+          options: {
+            presets: [ [ "@babel/preset-react", { runtime: "automatic" } ]] 
+          }
+        },
+        {
+          test: /\.(scss)$/,
+          use: [
+            {
+              // Adds CSS to the DOM by injecting a `<style>` tag
+              loader: 'style-loader'
+            },
+            {
+              // Interprets `@import` and `url()` like `import/require()` and will resolve them
+              loader: 'css-loader'
+            },
+            {
+              // Loader for webpack to process CSS with PostCSS
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    autoprefixer
+                  ]
+                }
+              }
+            },
+            {
+              // Loads a SASS/SCSS file and compiles it to CSS
+              loader: 'sass-loader'
+            }
+          ]
+        }
+      ]
+    }
+  };
+}
