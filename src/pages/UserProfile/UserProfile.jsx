fbb4053df436d20f95214af2889564a08664eb80
Hanna Kurhuzenkava <hanna_kurhuzenkava@epam.com>
2024-02-29 21:33:48 +0300

    MAIN-T-71 Update User profile UI

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import useUserInfoQuery from "../Home/useUserInfoQuery";
+import EditUserProfileForm from "./EditUserProfileForm";
 
 export default () => {
   const {isLoading, data} = useUserInfoQuery();
 
   return (
-      <div className="d-flex align-items-center justify-content-center">
-        Hello {isLoading ? '-' : data.uid}
+    <>
+      <div
+        className="d-flex flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 className="h3">
+          <span className="align-middle">Your Profile</span>
+        </h1>
       </div>
+      <div>
+        {!isLoading && <EditUserProfileForm document={data}/>}
+      </div>
+    </>
   )
 }
