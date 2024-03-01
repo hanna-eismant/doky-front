e1c02b7efd62ecc642848b28f0de330cde0861dd
Hanna Kurhuzenkava <hanna_kurhuzenkava@epam.com>
2024-03-01 11:15:58 +0300

    MAIN-T-71 Update User profile UI

--- src/pages/UserProfile/EditUserProfileForm/EditUserProfileForm.jsx
+++ src/pages/UserProfile/EditUserProfileForm/EditUserProfileForm.jsx
@@ -5,7 +5,7 @@ import {useAddToast} from "../../../components/Toasts";
 import useEditUserProfileQuery from "./useEditUserProfileQuery";
 
 export default ({user}) => {
-  const { data, fields: { uid, name, password } } = useFormData(user);
+  const { data, fields: { uid, name } } = useFormData(user);
   const [ editUserProfile, { isLoading } ] = useEditUserProfileQuery();
   const addToast = useAddToast();
 
@@ -24,7 +24,6 @@ export default ({user}) => {
     <form onSubmit={onSubmit} className="mt-3">
       <HorizontalFormInput id="uid" label="Email" type="text" value={data.uid} disabled={true} onChange={uid.setValue}/>
       <HorizontalFormInput id="name" label="Name" type="text" value={data.name} onChange={name.setValue}/>
-      <HorizontalFormInput id="password" label="Password" type="text" value={data.password} onChange={password.setValue}/>
       <div className="d-flex justify-content-between py-2">
         <input type="submit" value="Save" disabled={isLoading} className="btn btn-primary mb-3 float-right"/>
       </div>
