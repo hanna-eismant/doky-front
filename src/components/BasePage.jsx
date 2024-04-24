76c1c408dbe36c53f58891ff91ba9d5091d977ff
Hanna Kurhuzenkava <hanna-eismant@users.noreply.github.com>
2024-04-24 13:15:55 +0300

    MAIN-T-109 Add BE version badge on front (#18)

--- src/components/BasePage.jsx
+++ src/components/BasePage.jsx
@@ -8,6 +8,8 @@ export default ({children}) => {
         <div className="container-fluid">
           <Link className="navbar-brand text-white" to="/">
             <img height="40px" src="logo-white-no-bg.svg"/>
+            <img alt="Endpoint Badge" className="version-badge"
+                 src="https://img.shields.io/endpoint?url=https%3A%2F%2Fdoky-dev.azurewebsites.net%2Fversion"/>
           </Link>
         </div>
       </header>
