ccafc6b67e208c6e1a1bf9f113c6939fa7fb83cc
Anton <kurguzenkov@gmail.com>
2024-04-26 02:02:48 +0300

    MAIN-T-86 use useMutation for document creation

--- src/api/documents.js
+++ src/api/documents.js
@@ -1,5 +1,7 @@
-import { get } from './request';
+import { get, post } from './request';
 
 export const fetchDocuments = () => get('documents');
 
 export const fetchDocument = id => get(`documents/${id}`);
+
+export const createDocument = payload => post('documents', payload);
