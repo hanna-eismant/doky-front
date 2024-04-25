884c18930e2eceb09ae72299554baaca78302562
Anton <kurguzenkov@gmail.com>
2024-04-26 02:19:06 +0300

    MAIN-T-86 useMutation for document update

--- src/api/documents.js
+++ src/api/documents.js
@@ -1,7 +1,9 @@
-import { get, post } from './request';
+import { get, post, put } from './request';
 
 export const fetchDocuments = () => get('documents');
 
 export const fetchDocument = id => get(`documents/${id}`);
 
 export const createDocument = payload => post('documents', payload);
+
+export const updateDocument = payload => put(`documents/${payload.id}`, payload);
