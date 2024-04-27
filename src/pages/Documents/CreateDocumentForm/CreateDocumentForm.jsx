6a076f1a0d00594aedbdf034018090d349355461
Hanna Kurhuzenkava <hanna-eismant@users.noreply.github.com>
2024-04-27 18:56:05 +0300

    MAIN-T-124 It's possible to crate document with empty name and description (#21)
    * Implement form field validation and error handling
    
    Enhanced the forms in the `CreateDocument` and `EditDocument` components to handle field-level validation errors. Added `useState` hooks to capture global and field-specific errors. Applied conditional CSS classes to input fields to display invalid fields. Also, modified the form to show validation messages.

--- src/pages/Documents/CreateDocumentForm/CreateDocumentForm.jsx
+++ src/pages/Documents/CreateDocumentForm/CreateDocumentForm.jsx
@@ -1,7 +1,8 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useState} from 'react';
 import useFormData from "../../../hooks/useFormData";
 import HorizontalFormInput from "../../../components/formComponents/HorizontalFormInput.jsx";
 import HorizontalFormText from "../../../components/formComponents/HorizontalFormText.jsx";
+import { useAddToast } from '../../../components/Toasts';
 import { useMutation } from '../../../hooks/useMutation.js';
 import { createDocument } from '../../../api/documents.js';
 
@@ -11,23 +12,32 @@ const initialFormData = {
 };
 
 export default ({onCreated}) => {
+  const [fieldsError, setFieldsError] = useState({})
   const {data, fields: {name, description}} = useFormData(initialFormData);
   const [ documentMutation ] = useMutation(createDocument);
+  const addToast = useAddToast();
 
   const onSubmit = useCallback(async event => {
     event.preventDefault();
 
     const response = await documentMutation(data);
     if (response?.error) {
-      alert(response.error.message);
+      addToast(response.error.message)
+      setFieldsError({fields: response.fields})
     } else {
+      addToast('Created')
       onCreated();
     }
   });
 
+  const useFieldError = (fieldName) => {
+    return fieldsError?.fields?.find(({field}) => field === fieldName)
+  }
+
   return (
     <form onSubmit={onSubmit} className="mt-3">
-      <HorizontalFormInput id="name" label="Name" type="text" value={data.name} onChange={name.setValue}/>
+      <HorizontalFormInput id="name" label="Name" type="text" value={data.name} onChange={name.setValue}
+                           validationError={useFieldError('name')}/>
       <HorizontalFormText id="description" label="Description" value={data.description}
                           onChange={description.setValue}/>
       <div className="d-flex justify-content-between py-2">
