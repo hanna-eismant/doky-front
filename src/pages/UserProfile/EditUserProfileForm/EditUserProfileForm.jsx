882b10e9458426aeb02650822b7a4400e2ca83eb
Anton <kurguzenkov@gmail.com>
2024-04-28 23:55:55 +0300

    MAIN-T-136 use useForm form edit user profile form

--- src/pages/UserProfile/EditUserProfileForm/EditUserProfileForm.jsx
+++ src/pages/UserProfile/EditUserProfileForm/EditUserProfileForm.jsx
@@ -1,34 +1,31 @@
-import {useCallback} from 'react';
+import React from 'react';
+
 import HorizontalFormInput from '../../../components/formComponents/HorizontalFormInput.jsx';
-import { useFormData } from '../../../hooks/useFormData';
 import {useAddToast} from '../../../components/Toasts';
 import { useMutation } from '../../../hooks/useMutation.js';
 import { updateCurrentUser } from '../../../api/users.js';
+import { useForm } from '../../../hooks/useForm.js';
+import AlertError from '../../../components/AlertError.jsx';
 
 const EditUserProfileForm = ({ user }) => {
-  const { data, fields: { uid, name } } = useFormData(user);
-  const [ editUserProfile, { isLoading } ] = useMutation(updateCurrentUser);
+  const [ editUserProfile, { isLoading }, globalError ] = useMutation(updateCurrentUser);
   const addToast = useAddToast();
 
-  const onSubmit = useCallback(async event => {
-    event.preventDefault();
-
-    const response = await editUserProfile(data);
-    if (response?.error) {
-      alert(response.error.message);
-    } else {
-      addToast('Saved');
-    }
-  }, [addToast, data, editUserProfile]);
+  const { data, fields: { uid, name }, handleSubmit } = useForm(user, editUserProfile, () => {
+    addToast('Saved');
+  });
 
   return (
-    <form onSubmit={onSubmit} className="mt-3">
-      <HorizontalFormInput id="uid" label="Email" type="text" value={data.uid} disabled={true} onChange={uid.setValue}/>
-      <HorizontalFormInput id="name" label="Name" type="text" value={data.name} onChange={name.setValue}/>
-      <div className="d-flex justify-content-between py-2">
-        <input type="submit" value="Save" disabled={isLoading} className="btn btn-primary mb-3 float-right"/>
-      </div>
-    </form>
+    <>
+      {globalError ? <AlertError message={globalError} /> : null}
+      <form onSubmit={handleSubmit} className="mt-3">
+        <HorizontalFormInput id="uid" label="Email" type="text" value={data.uid} disabled={true} onChange={uid.setValue}/>
+        <HorizontalFormInput id="name" label="Name" type="text" value={data.name} onChange={name.setValue}/>
+        <div className="d-flex justify-content-between py-2">
+          <input type="submit" value="Save" disabled={isLoading} className="btn btn-primary mb-3 float-right"/>
+        </div>
+      </form>
+    </>
   );
 };
 
