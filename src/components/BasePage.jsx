340bfaf319d2a71024cde10b82094c50dc22f401
Hanna Kurhuzenkava <hanna-eismant@users.noreply.github.com>
2024-05-08 22:46:51 +0300

    MAIN-T-139 Migrate to Azure Spring App (#29)
    Update development server URL
    
    The base URL for the development server has been updated in the `config.dev.js` file. The change was made to reflect the move from 'doky-dev.azurewebsites.net' to the new server location on 'server.blackfield-1e13811b.westeurope.azurecontainerapps.io'.

--- src/components/BasePage.jsx
+++ src/components/BasePage.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const BasePage = ({ children }) => (
+const BasePage = ({children}) => (
   <>
     <header className="navbar bg-primary text-white bg-gradient sticky-top">
       <div className="container-fluid">
         <Link className="navbar-brand text-white" to="/">
           <img height="40px" src="/logo-white-no-bg.svg"/>
-          <img alt="Endpoint Badge" className="version-badge"
-            src="https://img.shields.io/endpoint?url=https%3A%2F%2Fdoky-dev.azurewebsites.net%2Fversion"/>
+          <span className="version-badge">
+            <img
+              alt="Endpoint Badge"
+              src="https://img.shields.io/endpoint?url=https%3A%2F%2Fserver.blackfield-1e13811b.westeurope.azurecontainerapps.io%2Fversion"
+            />
+          </span>
         </Link>
       </div>
     </header>
