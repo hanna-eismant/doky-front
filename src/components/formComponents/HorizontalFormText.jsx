0a42c512e708488cb156b8e75bfd8103d9734ed8
Hanna Kurhuzenkava <hanna-eismant@users.noreply.github.com>
2024-06-03 13:37:20 +0300

    MAIN-T-152 Edit Document Page Style (#31)
    * MAIN-T-152 Edit Document Page Style
    
    Refactor form components and adjust layout
    
    This commit introduces the implementation of the `countRows` function in `HorizontalFormText` used to dynamically adjust text area height. Both `HorizontalFormText` and `HorizontalFormInput` components' structure has been simplified without rows. On the `EditDocumentForm` page layout, details of document creator and editor are moved to an own column for better visibility.
    
    ---------
    
    Co-authored-by: Anton <kurguzenkov@gmail.com>

--- src/components/formComponents/HorizontalFormText.jsx
+++ src/components/formComponents/HorizontalFormText.jsx
@@ -1,18 +1,34 @@
-import React, {useCallback} from 'react';
-import {noop} from '../../utils';
+import React, {useCallback, useMemo} from 'react';
+import {noop, getRowsCount, clamp} from '../../utils';
 
-const HorizontalFormText = ({label, id, value = '', onChange = noop}) => {
+const getClampedRowsCount = (str, min, max) => {
+  const rowsCount = getRowsCount(str);
+  return clamp(rowsCount, min, max);
+};
+
+const HorizontalFormText = ({
+  label,
+  id,
+  minRows = 3,
+  maxRows = 10,
+  value = '',
+  onChange = noop
+}) => {
+
   const handleOnChange = useCallback(event => {
     event.preventDefault();
     onChange(event.target.value);
   }, [onChange]);
 
+  const rowsCount = useMemo(
+    () => getClampedRowsCount(value, minRows, maxRows),
+    [ value, minRows, maxRows]
+  );
+
   return (
-    <div className="row mb-3">
-      <label className="col-sm-2 col-form-label" htmlFor={id}>{label}:</label>
-      <div className="col-sm-10">
-        <textarea className="form-control" rows={3} id={id} value={value} onChange={handleOnChange}/>
-      </div>
+    <div className="mt-4">
+      <label className="form-label" htmlFor={id}>{label}:</label>
+      <textarea className="form-control" rows={rowsCount} id={id} value={value} onChange={handleOnChange}/>
     </div>
   );
 };
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,16 @@
 export const noop = () => {};
+
+export const getRowsCount = str =>
+  str.split('\n').length;
+
+export const clamp = (value, min, max) => {
+  if (value < min) {
+    return min;
+  }
+
+  if (value > max) {
+    return max;
+  }
+
+  return value;
+};
